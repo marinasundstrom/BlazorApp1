b364454dadd012234617d6e0106e3a855dba8c38
Marina Sundström <robert.sundstrom@outlook.com>
2022-04-30 01:35:35 +0200

    Add splash screen

--- Client/wwwroot/js/app.js
+++ Client/wwwroot/js/app.js
@@ -3,8 +3,6 @@ window.blazorCulture = {
     set: (value) => window.localStorage['BlazorCulture'] = value
 };
 
-
-
 window.isDarkMode = () => {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         return true;
@@ -15,3 +13,16 @@ window.isDarkMode = () => {
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', async event => {
     await DotNet.invokeMethodAsync("BlazorApp1.Client", "OnDarkModeChanged", event.matches);
 });
+
+function splashscreen() {
+    let preferredColorScheme = JSON.parse(window.localStorage["preferredColorScheme"] ?? "null");
+    let colorScheme = preferredColorScheme ?? (window.isDarkMode() ? 1 : 0);
+
+    if(colorScheme == 1) 
+    {
+        const elem = document.getElementById("splashscreen");
+        elem.style.backgroundColor = "#27272fff"; 
+    }
+}
+
+splashscreen();
